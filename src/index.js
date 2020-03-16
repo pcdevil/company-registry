14f2e65ebbabd048a2a46d08cee1ce23a9c62ef6
Attila Gonda <pcdevil@users.noreply.github.com>
2020-03-16 17:58:53 +0100

    feat(company-registry): add api framework

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log('Hello, World');
+const fastifyFactory = require('fastify');
+
+const fastify = fastifyFactory({ logger: true });
+
+fastify.get('/', async () => {
+	return { message: 'Hello, World' };
+});
+
+async function start () {
+	try {
+		await fastify.listen(3000);
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+}
+
+start();

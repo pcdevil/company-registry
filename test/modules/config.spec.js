3a80b59902838e004b3b1adda71700699a4259fc
Attila Gonda <pcdevil@users.noreply.github.com>
2020-03-17 16:32:59 +0100

    test(config): use data-provider pattern for very similar tests

--- test/modules/config.spec.js
+++ test/modules/config.spec.js
@@ -24,91 +24,40 @@ describe('Config', () => {
 		populatedDotenv = { config: sinon.stub().returns({ parsed: envStub }) };
 	});
 
-	describe('.mongodb', () => {
-		it('should handle errors from the dotenv module', () => {
-			const subject = new Config(errorDotenv);
+	const configProperties = {
+		mongodb: [
+			{ property: 'host', env: 'MONGODB_HOST', defaultValue: 'mongodb' },
+			{ property: 'port', env: 'MONGODB_PORT', defaultValue: 27017 },
+			{ property: 'database', env: 'MONGODB_DATABASE', defaultValue: 'company-registry' },
+		],
+		server: [
+			{ property: 'host', env: 'SERVER_HOST', defaultValue: 'localhost' },
+			{ property: 'port', env: 'SERVER_PORT', defaultValue: 8080 },
+			{ property: 'logger', env: 'SERVER_LOGGER', defaultValue: true },
+		],
+	};
 
-			expect(subject.mongodb).to.be.an('object');
+	for (const [category, properties] of Object.entries(configProperties)) {
+		describe(`.${category}`, () => {
+			it('should handle errors from the dotenv module', () => {
+				const subject = new Config(errorDotenv);
+
+				expect(subject[category]).to.be.an('object');
+			});
+
+			for (const { property, env, defaultValue } of properties) {
+				it(`should have a "${property}" property populated by the "${env}" env variable`, () => {
+					const subject = new Config(populatedDotenv);
+
+					expect(subject[category][property]).to.be.eql(envStub[env]);
+				});
+
+				it(`should have a "${property}" property with default value if the "${env}" env variable is unset`, () => {
+					const subject = new Config(emptyDotenv);
+
+					expect(subject[category][property]).to.be.eql(defaultValue);
+				});
+			}
 		});
-
-		it('should have a "host" property populated by the "MONGODB_HOST" env variable', () => {
-			const subject = new Config(populatedDotenv);
-
-			expect(subject.mongodb.host).to.be.eql(envStub.MONGODB_HOST);
-		});
-
-		it('should have a "host" property with default value if the "MONGODB_HOST" env variable is unset', () => {
-			const subject = new Config(emptyDotenv);
-
-			expect(subject.mongodb.host).to.be.eql('mongodb');
-		});
-
-		it('should have a "port" property populated by the "MONGODB_PORT" env variable', () => {
-			const subject = new Config(populatedDotenv);
-
-			expect(subject.mongodb.port).to.be.eql(envStub.MONGODB_PORT);
-		});
-
-		it('should have a "port" property with default value if the "MONGODB_PORT" env variable is unset', () => {
-			const subject = new Config(emptyDotenv);
-
-			expect(subject.mongodb.port).to.be.eql(27017);
-		});
-
-		it('should have a "database" property populated by the "MONGODB_DATABASE" env variable', () => {
-			const subject = new Config(populatedDotenv);
-
-			expect(subject.mongodb.database).to.be.eql(envStub.MONGODB_DATABASE);
-		});
-
-		it('should have a "database" property with default value if the "MONGODB_DATABASE" env variable is unset', () => {
-			const subject = new Config(emptyDotenv);
-
-			expect(subject.mongodb.database).to.be.eql('company-registry');
-		});
-	});
-
-	describe('.server', () => {
-		it('should handle errors from the dotenv module', () => {
-			const subject = new Config(errorDotenv);
-
-			expect(subject.server).to.be.an('object');
-		});
-
-		it('should have a "host" property populated by the "SERVER_HOST" env variable', () => {
-			const subject = new Config(populatedDotenv);
-
-			expect(subject.server.host).to.be.eql(envStub.SERVER_HOST);
-		});
-
-		it('should have a "host" property with default value if the "SERVER_HOST" env variable is unset', () => {
-			const subject = new Config(emptyDotenv);
-
-			expect(subject.server.host).to.be.eql('localhost');
-		});
-
-		it('should have a "port" property populated by the "SERVER_PORT" env variable', () => {
-			const subject = new Config(populatedDotenv);
-
-			expect(subject.server.port).to.be.eql(envStub.SERVER_PORT);
-		});
-
-		it('should have a "port" property with default value if the "SERVER_PORT" env variable is unset', () => {
-			const subject = new Config(emptyDotenv);
-
-			expect(subject.server.port).to.be.eql(8080);
-		});
-
-		it('should have a "logger" property populated by the "SERVER_LOGGER" env variable', () => {
-			const subject = new Config(populatedDotenv);
-
-			expect(subject.server.logger).to.be.eql(envStub.SERVER_LOGGER);
-		});
-
-		it('should have a "logger" property with default value if the "SERVER_LOGGER" env variable is unset', () => {
-			const subject = new Config(emptyDotenv);
-
-			expect(subject.server.logger).to.be.eql(true);
-		});
-	});
+	}
 });

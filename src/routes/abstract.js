995756a0d68b027dbac998c78b26353f9d434204
Attila Gonda <pcdevil@users.noreply.github.com>
2020-03-20 11:02:07 +0100

    feat(abstract-route): add support for schema

--- src/routes/abstract.js
+++ src/routes/abstract.js
@@ -3,9 +3,10 @@
 const { NotImplementedError } = require('../lib');
 
 class AbstractRoute {
-	constructor (method, url) {
+	constructor (method, url, schema = {}) {
 		this._method = method;
 		this._url = url;
+		this._schema = schema;
 	}
 
 	static createDefault () {
@@ -15,8 +16,9 @@ class AbstractRoute {
 	getOptions () {
 		const method = this._method;
 		const url = this._url;
+		const schema = this._schema;
 		const handler = this._getHandler();
-		return { method, url, handler };
+		return { method, url, schema, handler };
 	}
 
 	_getHandler () {
--- test/routes/abstract.spec.js
+++ test/routes/abstract.spec.js
@@ -6,28 +6,43 @@ const { NotImplementedError } = require('../../src/lib');
 
 
 describe('AbstractRoute', () => {
+	class TestRoute extends AbstractRoute {
+		_getHandler () {
+			return handler;
+		}
+	}
+
 	const method = 'GET';
 	const url = '/';
+	const schema = {
+		body: {
+			type: 'object',
+			properties: { name: { type: 'string' } },
+		},
+	};
 	const handler = async () => ({ test: true });
 
 	describe('getOptions()', () => {
 		it('should throw an error when the _getHandler method is not implemented', () => {
-			const subject = new AbstractRoute(method, url);
+			const subject = new AbstractRoute(method, url, schema);
 
 			expect(() => subject.getOptions()).to.throw(NotImplementedError);
 		});
 
-		it('should return the properties as an object when the _getHandler is implemented', () => {
-			class TestRoute extends AbstractRoute {
-				_getHandler () {
-					return handler;
-				}
-			}
+		it('should return the properties as an object', () => {
+			const subject = new TestRoute(method, url, schema);
+
+			const actual = subject.getOptions();
+
+			expect(actual).to.be.eql({ method, url, schema, handler });
+		});
+
+		it('should return an empty schema if it was not defined', () => {
 			const subject = new TestRoute(method, url);
 
 			const actual = subject.getOptions();
 
-			expect(actual).to.be.eql({ method, url, handler });
+			expect(actual.schema).to.be.eql({});
 		});
 	});
 });
